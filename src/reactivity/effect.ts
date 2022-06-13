3d5038682d49ee008ea90ca66652d4424461ff57
Sky-star <4179412258@qq.com>
2022-06-13 21:21:05 +0800

    重新手写了一遍reactive&effect以及scheduler,stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,61 +1,66 @@
-import { extend } from "../shared"
-
 class ReactiveEffect {
     private _fn: any
+    // 将对应的依赖进行反向收集
     deps = []
+    // 为了防止重复的进行停止监听，只进行一次就可以了
     active = true
     onStop?: () => void
-    constructor(fn,public scheduler?) {
+    constructor(fn) {
         this._fn = fn
     }
-    
+
     run() {
+        // 第一次执行时需要将当前的effect保存下来
         activeEffect = this
         return this._fn()
     }
 
     stop() {
         if (this.active) {
-            cleanupEffect(this)
+
+            // 说是stop，实际就是将这个runner从dep中删除了
+            this.deps.forEach((dep: any) => {
+                dep.delete(this)
+            })
+            
             if (this.onStop) {
                 this.onStop()
             }
+
+            // 执行一次就可以了
             this.active = false
         }
     }
 }
 
-function cleanupEffect(effect) {
-    effect.deps.forEach((dep: any) => {
-        dep.delete(effect)
-    });
-}
-
+// 依赖收集工作，所谓的依赖收集就是将当前对应的target下的key中的effect保存起来
+// 留着set被触发时再次执行
+let activeEffect
 const targetMap = new Map()
 export function track(target, key) {
+    // 构造一个存储target容器
     let depsMap = targetMap.get(target)
     if (!depsMap) {
         depsMap = new Map()
         targetMap.set(target, depsMap)
     }
 
-    let dep = depsMap.get(key)
-    if (!dep) {
-        dep = new Set()
-        depsMap.set(key, dep)
+    let map = depsMap.get(key)
+    if (!map) {
+        map = new Set()
+        depsMap.set(key, map)
     }
 
-    if (!activeEffect) return
-    
-    dep.add(activeEffect)
-    activeEffect.deps.push(dep)
+    map.add(activeEffect)
+    // 将当前的deps进行反向收集 方便日后删除使用
+    activeEffect.deps.push(map)
 }
 
 export function trigger(target, key) {
-    let depsMap = targetMap.get(target)
-    let dep = depsMap.get(key)
+    const depsMap = targetMap.get(target)
+    const map = depsMap.get(key)
 
-    for (const effect of dep) {
+    for (const effect of map) {
         if (effect.scheduler) {
             effect.scheduler()
         } else {
@@ -64,20 +69,26 @@ export function trigger(target, key) {
     }
 }
 
-let activeEffect
-export function effect(fn, options: any = {}) {
-    const _effect = new ReactiveEffect(fn, options.scheduler)
-    // options
-    extend(_effect, options)
-    
+export function stop(runner) {
+    runner.effect.stop()
+}
+
+export function effect(fn, options?) {
+
+    // 创建一个effect对象
+    const _effect = new ReactiveEffect(fn)
+
+    // 将传进来的参数赋值给_effect
+    Object.assign(_effect, options)
+
+    // 执行一次fn方法
     _effect.run()
 
+    // 如何将runner返回，将effect中run函数利用bind方法复制一份出来
+    // bind() 方法创建一个新的函数，在 bind() 被调用时，这个新函数的 this 被指定为 bind() 的第一个参数，而其余参数将作为新函数的参数，供调用时使用
     const runner: any = _effect.run.bind(_effect)
+    // 这是为了给stop使用的 将effect反向赋值给effect，从而能够不在监听
     runner.effect = _effect
 
     return runner
-}
-
-export function stop(runner) {
-    runner.effect.stop()
 }
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,19 +1,22 @@
-import { track,trigger } from "./effect"
+import { track, trigger } from "./effect"
 
-export function reactive(raw) {
-    return new Proxy(raw, {
-        get(target, key) {
+export function reactive(obj) {
+    // 响应式对象，用proxy执行代理，并在获取数据时做一些操作从而达到响应式的结果
+    return new Proxy(obj, {
+        // 这是一个捕捉器
+        // Reflect基本能够匹配对象中对应的方法，并且不会存在this指向问题
+        get(target,key) {
+            // 返回对应的属性之后还能够进行一些其他操作
             const res = Reflect.get(target,key)
-            // 依赖收集
-            track(target, key)
+            // 这里进行依赖收集
+            track(target,key)
             return res
         },
-        set(target, key, value) {
-            const res = Reflect.set(target, key, value)
-            
-            // 触发依赖
-            trigger(target, key)
-            return res;
-        }
+        set(target,key,value) {
+            const res = Reflect.set(target,key,value)
+            // 这里进行依赖触发
+            trigger(target,key)
+            return res
+        } 
     })
 }
