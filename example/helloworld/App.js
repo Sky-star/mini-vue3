1fecfe232d7cc48c59fa30cecfe5014a07caa8af
白泉 <417941258@qq.com>
2022-06-30 10:56:41 +0800

    22-实现初始化element主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -2,7 +2,17 @@ import { h } from "../../lib/guide-mini-vue.esm.js"
 
 export const App = {
 	render() {
-		return h("div", "hi. " + this.msg)
+		return h(
+			"div",
+			{
+				id: "root",
+				class: ["red", "hard"]
+			},
+			// "hi. " + this.msg
+			// string
+			// "hi, mini-vue"
+			[h("p", { class: "red" }, "hi"), h("p", { class: "blue" }, "mini-vue")]
+		)
 	},
 
 	setup() {
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode, container) {
@@ -8,15 +9,44 @@ export function render(vnode, container) {
 function patch(vnode, container) {
     // TODO 判断vnode 是不是一个element
     // 是 element 那么就应该处理element
-    // processElement(vnode,container)
-    // 去处理组件
-    processComponent(vnode, container)
+    if (typeof vnode.type === 'string') {
+        processElement(vnode, container)
+    } else if (isObject(vnode.type)) {
+        // 去处理组件
+        processComponent(vnode, container)
+    }
+}
+
+function processElement(vnode: any, container: any) {
+    mountElement(vnode, container)
 }
 
 function processComponent(vnode: any, container: any) {
     mountComponent(vnode, container)
 }
 
+function mountElement(vnode: any, container: any) {
+    const el = document.createElement(vnode.type)
+    const { children, props } = vnode
+    if (typeof children === "string") {
+        el.textContent = children
+    } else if (Array.isArray(children)) {
+        mountChildren(vnode, el)
+    }
+
+    for (const key in props) {
+        const val = props[key]
+        el.setAttribute(key, val)
+    }
+
+    container.append(el)
+}
+
+function mountChildren(vnode, container) {
+    vnode.children.forEach((v) => {
+        patch(v, container)
+    });
+}
 
 function mountComponent(vnode: any, container) {
     const instance = createComponentInstance(vnode)
@@ -31,4 +61,5 @@ function setupRenderEffect(instance: any, container) {
 
     patch(subTree, container)
 }
+
 
