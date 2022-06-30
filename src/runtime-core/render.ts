7d4cc6ba00298d211a1be4099a72d7e888607545
白泉 <417941258@qq.com>
2022-06-30 16:34:04 +0800

    24-实现shapeFlags

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,4 +1,5 @@
 import { isObject } from "../shared/index"
+import { ShapeFlags } from "../shared/ShapeFlags"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode, container) {
@@ -7,11 +8,10 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-    // TODO 判断vnode 是不是一个element
-    // 是 element 那么就应该处理element
-    if (typeof vnode.type === 'string') {
+    const { shapeFlag } = vnode
+    if (shapeFlag & ShapeFlags.ELEMENT) {
         processElement(vnode, container)
-    } else if (isObject(vnode.type)) {
+    } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
         // 去处理组件
         processComponent(vnode, container)
     }
@@ -27,10 +27,12 @@ function processComponent(vnode: any, container: any) {
 
 function mountElement(vnode: any, container: any) {
     const el = (vnode.el = document.createElement(vnode.type))
-    const { children, props } = vnode
-    if (typeof children === "string") {
+
+    const { children, props, shapeFlag } = vnode
+
+    if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
         el.textContent = children
-    } else if (Array.isArray(children)) {
+    } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
         mountChildren(vnode, el)
     }
 
