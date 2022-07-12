5aa7b93297e02fcb97b83f556e294aa9512eb7bb
白泉 <417941258@qq.com>
2022-07-12 17:09:31 +0800

    27-实现组件emit功能

---
+++ example/componentEmit/App.js
@@ -0,0 +1,23 @@
+import { h } from "../../lib/guide-mini-vue.esm.js"
+import { Foo } from "./Foo.js"
+
+export const App = {
+	render() {
+		//emit
+		return h("div", {}, [
+			h("div", {}, "App"),
+			h(Foo, {
+				onAdd(a, b) {
+					console.log("onAdd", a, b)
+				},
+				onAddFoo(a, b) {
+					console.log("onAddFoo", a, b)
+				}
+			})
+		])
+	},
+
+	setup() {
+		return {}
+	}
+}
--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -1,4 +1,5 @@
 import { shallowReadonly } from "../reactivity/reactive"
+import { emit } from "./componentEmit"
 import { initProps } from "./componentProps"
 import { PublicInstanceHandlers } from "./componentPublicInstance"
 
@@ -7,9 +8,11 @@ export function createComponentInstance(vnode) {
         vnode,
         type: vnode.type,
         setupState: {},
-        props: {}
+        props: {},
+        emit: () => { }
     }
 
+    component.emit = emit.bind(null, component) as any
     return component
 }
 
@@ -30,7 +33,9 @@ function setupStatefulComponent(instance: any) {
     const { setup } = Component
 
     if (setup) {
-        const setupResult = setup(shallowReadonly(instance.props))
+        const setupResult = setup(shallowReadonly(instance.props), {
+            emit: instance.emit
+        })
 
         handleSetupResult(instance, setupResult)
     }
