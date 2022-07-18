4f9e3f63a83603cb77266c3d062b3aa2b12d4e2e
白泉 <417941258@qq.com>
2022-07-18 16:40:48 +0800

    34-更新element的props

--- example/update/App.js
+++ example/update/App.js
@@ -8,14 +8,37 @@ export const App = {
 			count.value++
 		}
 
+		const props = ref({
+			foo: "foo",
+			bar: "bar"
+		})
+
+		const onChangePropsDemo1 = () => {
+			props.value.foo = "new-foo"
+		}
+
+		const onChangePropsDemo2 = () => {
+			props.value.foo = undefined
+		}
+
+		const onChangePropsDemo3 = () => {
+			props.value = {
+				foo: "foo"
+			}
+		}
+
 		return {
 			count,
-			onClick
+			onClick,
+			onChangePropsDemo1,
+			onChangePropsDemo2,
+			onChangePropsDemo3,
+			props
 		}
 	},
 
 	render() {
-		return h("div", { id: "root" }, [
+		return h("div", { id: "root", ...this.props }, [
 			h("div", {}, "count:" + this.count),
 			h(
 				"button",
@@ -23,6 +46,27 @@ export const App = {
 					onClick: this.onClick
 				},
 				"click"
+			),
+			h(
+				"button",
+				{
+					onClick: this.onChangePropsDemo1
+				},
+				"changeProps - 值改变了 - 修改"
+			),
+			h(
+				"button",
+				{
+					onClick: this.onChangePropsDemo2
+				},
+				"changeProps - 值改变成了 undefined - 删除"
+			),
+			h(
+				"button",
+				{
+					onClick: this.onChangePropsDemo3
+				},
+				"changeProps - key在新的里面没有了 - 删除"
 			)
 		])
 	}
