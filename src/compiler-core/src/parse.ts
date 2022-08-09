263e5250ae7eaf1b5a226307fbccc5cedd229c87
白泉 <417941258@qq.com>
2022-08-09 09:11:38 +0800

    45-实现解析text功能

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -24,8 +24,30 @@ function parseChildren(context) {
         }
     }
 
+    if (!node) {
+        node = parseText(context)
+    }
+
     nodes.push(node)
     return nodes
+}
+
+function parseText(context: any) {
+
+    const content = parseTextData(context, context.source.length)
+
+    return {
+        type: NodeTypes.TEXT,
+        content
+    }
+}
+
+function parseTextData(context: any, length) {
+    const content = context.source.slice(0, length)
+
+    advanceBy(context, length)
+
+    return content
 }
 
 function parseElement(context: any) {
@@ -61,7 +83,7 @@ function parseInterpolation(context) {
 
     const rawContentLength = closeIndex - openDelimiter.length;
 
-    const rawContent = context.source.slice(0, rawContentLength)
+    const rawContent = parseTextData(context, rawContentLength)
 
     const content = rawContent.trim()
 
